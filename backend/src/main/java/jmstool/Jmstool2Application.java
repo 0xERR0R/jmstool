bc245f8c57842c97f88fbb1f358f17fab95e867a
Dimitri Herzog <dimitri.herzog@gmail.com>
2017-10-17 22:40:30 +0200

    removed main method

--- backend/src/main/java/jmstool/Jmstool2Application.java
+++ backend/src/main/java/jmstool/Jmstool2Application.java
@@ -1,7 +1,6 @@
 package jmstool;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -22,10 +21,6 @@ import jmstool.storage.LocalMessageStorage;
 @SpringBootApplication
 @EnableJms
 public class Jmstool2Application extends SpringBootServletInitializer {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Jmstool2Application.class, args);
-	}
 
 	@Bean
 	public TaskExecutor taskExecutor() {
